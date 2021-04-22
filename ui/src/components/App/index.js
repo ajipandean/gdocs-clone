1b2492caaa651b53a3566e5a3ea8f533f247e6c8
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2021-04-22 14:40:12 +0800

    Added toolbar

--- ui/src/components/App/index.js
+++ ui/src/components/App/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Editor from '../Editor'
+import Toolbar from '../Toolbar'
 
 export default function App () {
   return (
     <div>
+      <Toolbar />
       <Editor />
     </div>
   )
---
+++ ui/src/components/Toolbar/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Toolbar () {
+  return (
+    <header className="bg-white border-b py-2 px-4">
+      <div>
+        <h2 className="text-lg font-bold">Google Docs Clone</h2>
+        <p className="text-gray-600">by Muhamad Aji Pandean Mertayasa</p>
+      </div>
+    </header>
+  )
+}
